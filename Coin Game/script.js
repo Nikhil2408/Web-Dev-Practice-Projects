4d7c4561311b2613fdb094b6209774c7eb176ace
Nikhil Bansal <nikbansal1998@gmail.com>
2022-01-22 22:23:15 -0900

    added player and coin movement

---
+++ Coin Game/script.js
@@ -0,0 +1,63 @@
+function isTouching(a, b) {
+	const aRect = a.getBoundingClientRect();
+	const bRect = b.getBoundingClientRect();
+
+	return !(
+		aRect.top + aRect.height < bRect.top ||
+		aRect.top > bRect.top + bRect.height ||
+		aRect.left + aRect.width < bRect.left ||
+		aRect.left > bRect.left + bRect.width
+	);
+}
+
+let points = 0;
+const player = document.querySelector("#player");
+const coin = document.querySelector("#coin");
+const pointDisplay = document.querySelector("input[type='text']");
+document.body.addEventListener("keydown", function (eventObj)
+{
+	if(eventObj.key === "ArrowDown")
+	{
+		player.style.top = parseInt(getComputedStyle(player).top) + 50 + "px";
+		if(isTouching(coin, player))
+		{
+			coin.style.left = Math.floor(Math.random() * window.innerWidth) + "px";
+			coin.style.top = Math.floor(Math.random() * window.innerHeight) + "px";
+			points++;
+			pointDisplay.value = points;
+		}
+	}
+	if(eventObj.key === 'ArrowRight')
+	{
+		player.style.left = parseInt(getComputedStyle(player).left) + 50 + "px";
+		if(isTouching(coin, player))
+		{
+			coin.style.left = Math.floor(Math.random() * window.innerWidth) + "px";
+			coin.style.top = Math.floor(Math.random() * window.innerHeight) + "px";
+			points++;
+			pointDisplay.value = points;
+		}
+	}
+	if(eventObj.key === 'ArrowLeft')
+	{
+		player.style.left = parseInt(getComputedStyle(player).left) - 50 + "px";
+		if(isTouching(coin, player))
+		{
+			coin.style.left = Math.floor(Math.random() * window.innerWidth) + "px";
+			coin.style.top = Math.floor(Math.random() * window.innerHeight) + "px";
+			points++;
+			pointDisplay.value = points;
+		}
+ 	}
+	 if(eventObj.key === 'ArrowUp')
+	 {
+		player.style.top = parseInt(getComputedStyle(player).top) - 50 + "px";
+		if(isTouching(coin, player))
+		{
+			coin.style.left = Math.floor(Math.random() * window.innerWidth) + "px";
+			coin.style.top = Math.floor(Math.random() * window.innerHeight) + "px";
+			points++;
+			pointDisplay.value = points;
+		}
+	 }
+});
