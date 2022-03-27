6169bd5579baef1f58326ab5424c66bf04a7fe3d
Nikhil Bansal <nikbansal1998@gmail.com>
2022-03-27 20:43:49 +0530

    feat: operands and operator functionality added

--- Calculator/app.js
+++ Calculator/app.js
@@ -1,1 +1,40 @@
-console.log("Hello");
+const allBtns = document.querySelectorAll(".container > .calc-container > .row > button");
+
+const display = document.querySelector(".display > input[type='text']");
+
+
+let currentDisplay = '';
+for(let btn of allBtns)
+{
+    if(btn.classList.contains("operand"))
+    {
+        btn.addEventListener("click", function(){
+            currentDisplay = currentDisplay + btn.innerText;
+            display.value = currentDisplay;
+        })
+    }
+
+    if(btn.classList.contains("operator"))
+    {
+        btn.addEventListener("click", function(){
+            currentDisplay = currentDisplay + " " + btn.innerText + " ";
+            display.value = currentDisplay;
+        })
+    }
+
+    if(btn.classList.contains("equal"))
+    {
+        btn.addEventListener("click", function(){
+            display.value = eval(display.value);
+            currentDisplay = '';
+        })
+    }
+
+    if(btn.classList.contains("reset"))
+    {
+        btn.addEventListener("click", function(){
+            currentDisplay = '';
+            display.value = 0;
+        })
+    }
+}
