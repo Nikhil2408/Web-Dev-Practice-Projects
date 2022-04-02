df0a3d56390d5274d213bd58bcfece15a4d92b3c
Nikhil Bansal <nikbansal1998@gmail.com>
2022-04-02 18:08:37 +0530

    feat: Added local storage for seat Selected

--- Basic Movie Booking System/app.js
+++ Basic Movie Booking System/app.js
@@ -2,7 +2,17 @@ const movieSelected = document.querySelector("#movies");
 const seats = document.querySelectorAll(".seat");
 const displayElement = document.querySelector(".main-container > .displayPrice > h3");
 
-let seatsSelected = 0;
+if(localStorage.getItem("seatsStored"))
+{
+    const seatsStored = localStorage.getItem("seatsStored").replaceAll("[", "").replaceAll("]", "").split(",");
+
+    seatsStored.forEach(function(seatStored){
+        seats[parseInt(seatStored)].classList.toggle("selected");
+    });
+}
+
+let seatsSelectedCount = 0;
+let seatsSelected;
 let totalPrice = 0;
 seats.forEach(function(seat){
     seat.addEventListener("click", function(){
@@ -16,17 +26,26 @@ seats.forEach(function(seat){
 });
 
 
-movieSelected.addEventListener("change", function(){
+movieSelected.addEventListener("change", function(eventObj){
     calculatePrice();
     updateDisplayElement();
 })
 
 function calculatePrice()
 {
-    seatsSelected = document.querySelectorAll(".main-container .movie-book-container .seats-container .selected").length;
-    totalPrice = parseInt(movieSelected.value) * seatsSelected;
+    seatsSelected = document.querySelectorAll(".main-container .movie-book-container .seats-container .selected");
+    seatsSelectedCount = seatsSelected.length;
+    totalPrice = parseInt(movieSelected.value) * seatsSelectedCount;
+    
+    let seatsSelectedIndex = [...seatsSelected].map(function(seatSelected){
+        return [...seats].indexOf(seatSelected);
+    });
+    
+    localStorage.setItem("seatsStored", JSON.stringify(seatsSelectedIndex));
 }
 
 function updateDisplayElement(){
-    displayElement.innerText = `You have selected ${seatsSelected} seats. Total Price ${totalPrice}`;
+    displayElement.innerHTML = `You have selected <span> ${seatsSelectedCount} </span> seats. Total Price <span> Rs. ${totalPrice} </span>`;
 }
+
+
